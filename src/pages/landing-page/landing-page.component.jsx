eb62f95fc907cae82ec2929b6d380a8e92bc23a5
Aniruddh Upparna <aniruddhupparna@gmail.com>
2021-07-17 12:36:11 +0530

    added landing page

--- src/pages/landing-page/landing-page.component.jsx
+++ src/pages/landing-page/landing-page.component.jsx
@@ -3,6 +3,7 @@ import './landing-page.styles.css';
 import Button from '@material-ui/core/Button';
 import Logo from '../../assets/gb-logo-dark.jpg';
 import { Link } from 'react-router-dom';
+// import LandingBg from '../../assets/landing-bg.jpg';
 
 const LandingPage = () =>
     (<div className="landing-page-container">
@@ -12,12 +13,14 @@ const LandingPage = () =>
           </div>
           <div className="landing-title">GOODIE BAG STUDIO</div>
       </div>
-      <div className="projects-btn-container">
+      <div className="landing-lower">
+        <div className="landing-btn-container">
         <Link to="/projects">
           <Button variant="contained" color="primary">
             View projects
           </Button>
         </Link>
+        </div>
       </div>
     </div>)
 
